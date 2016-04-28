9aff61353d501142e79ac5b4211c0c4b7da59760
Yuvraj <Yuvraj@192.168.0.4>
2016-04-29 02:20:59 +0530

    First commit to git

---
+++ src/main/java/com/yuvi/yapp/YAppApplication.java
@@ -0,0 +1,12 @@
+package com.yuvi.yapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YAppApplication.class, args);
+	}
+}
